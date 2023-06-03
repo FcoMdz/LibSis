378b341c9ef4754ad14cb10481caaf6737063108
Gerardo12002 <fematdelgado.github@gmail.com>
2023-06-03 17:12:59 -0600

    subiendo la distribucion para probar

--- configuraciones/rutas.js
+++ configuraciones/rutas.js
@@ -10,4 +10,20 @@ router.get('/', (req, res) => {
     console.log("Enviado: " + link);
 });
 
+router.post('/RegProd', (req, res) => {
+    
+});
+
+router.post('/CrearNV', (req, res) => {
+
+});
+
+router.post('/ConsNV', (req, res) => {
+
+});
+
+router.post('/IniciarSesion', (req, res) => {
+
+});
+
 module.exports = router;
