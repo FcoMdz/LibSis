5454c37567e57e10846f069aa5f6419cdd2842d7
Gerardo12002 <fematdelgado.github@gmail.com>
2023-06-03 18:42:11 -0600

    configurando

--- Base de datos/libreria.sql
+++ Base de datos/libreria.sql
@@ -1,3 +1,5 @@
+USE railway;
+
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
@@ -5,7 +7,6 @@
 -- Servidor: 127.0.0.1
 -- Tiempo de generación: 04-06-2023 a las 00:27:24
 -- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
