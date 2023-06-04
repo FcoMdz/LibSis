1aef60214197852b1ef744306e37c1e7d704f6db
Gerardo12002 <fematdelgado.github@gmail.com>
2023-06-03 19:39:09 -0600

    allowing cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const rutas = require('./configuraciones/rutas');
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
