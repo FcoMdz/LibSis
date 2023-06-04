74892dc3da2e96fcf867da57d2558723e01cf2eb
Gerardo12002 <fematdelgado.github@gmail.com>
2023-06-03 18:43:38 -0600

    save

--- configuraciones/rutas.js
+++ configuraciones/rutas.js
@@ -5,7 +5,7 @@ const sql = require("./conection");
 
 router.use(express.static(process.cwd()+'/AngularProyect/dist/lib-sis/'))
 router.get('/', (req, res) => {
-    link = process.cwd()+ '/AngularProyect/dist/lib-sis/';
+    var link = process.cwd()+ '/AngularProyect/dist/lib-sis/';
     res.sendFile(link);
     console.log("Enviado: " + link);
 });
