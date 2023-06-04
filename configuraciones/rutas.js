cfd451bd7083783f3f2c25952dce0f636e30a7cc
Gerardo12002 <fematdelgado.github@gmail.com>
2023-06-03 19:38:33 -0600

    no conection between angular and node

--- configuraciones/rutas.js
+++ configuraciones/rutas.js
@@ -3,13 +3,6 @@ const express = require('express');
 const router = express.Router();
 const sql = require("./conection");
 
-router.use(express.static(process.cwd()+'/AngularProyect/dist/lib-sis/'));
-router.get('/', (req, res) => {
-    var link = process.cwd()+'/AngularProyect/dist/lib-sis/';
-    res.sendFile(link);
-    console.log("Enviado: " + link);
-});
-
 router.post('/RegProd', (req, res) => {
     
 });
