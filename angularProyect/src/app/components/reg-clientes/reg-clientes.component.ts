df476de3a99e966a2ab269c2f28bfa194b32f62e
Emmanuel Munoz <emunozce80@gmail.com>
2023-11-27 08:16:53 -0600

    modulo de registro de clientes

---
+++ angularProyect/src/app/components/reg-clientes/reg-clientes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-reg-clientes',
+  templateUrl: './reg-clientes.component.html',
+  styleUrls: ['./reg-clientes.component.css'],
+})
+export class RegClientesComponent {
+  usuario: any = sessionStorage.getItem('usuario');
+
+  formGroup = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    telefono: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'),
+    ]),
+  });
+}
