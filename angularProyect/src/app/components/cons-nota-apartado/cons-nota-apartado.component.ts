387669748ec0a2242fe034bb65dc271c5601eabb
Emmanuel Munoz <emunozce80@gmail.com>
2023-11-27 09:26:17 -0600

    Creacion de consulta de nota_venta y nota_apartado

---
+++ angularProyect/src/app/components/cons-nota-apartado/cons-nota-apartado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cons-nota-apartado',
+  templateUrl: './cons-nota-apartado.component.html',
+  styleUrls: ['./cons-nota-apartado.component.css']
+})
+export class ConsNotaApartadoComponent {
+
+}
